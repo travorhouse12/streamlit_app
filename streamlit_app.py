2c799fe2e1ed1150ac2ff76b1b7d073bcff80dd1
Travor House <63683715+travorhouse12@users.noreply.github.com>
2023-10-25 20:59:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ streamlit.text("Turtles are green and live in a ponds and lakes")
 fruits = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits = fruits.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits:", list(fruits.index))
+streamlit.multiselect("Pick some fruits:", list(fruits.index), ['Avocado', 'Strawberries'])
 streamlit.dataframe(fruits)
