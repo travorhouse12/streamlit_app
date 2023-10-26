1e8ec6d9fb2819168076731b1faf46a94ef6d197
Travor House <63683715+travorhouse12@users.noreply.github.com>
2023-10-25 21:19:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,14 @@
 import streamlit
 import pandas as pd
 
-#streamlit.title("I love turtle")
-#streamlit.text("Turtles are green and live in a ponds and lakes")
+streamlit.title("I love turtle")
+streamlit.text("Turtles are green and live in a ponds and lakes")
 
-#fruits = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#fruits = fruits.set_index('Fruit')
-
-#all_fruits = fruits.index.unique().tolist()
-
-#fruits_selected = streamlit.multiselect("Pick some fruits:", list(fruits.index), all_fruits)
-#fruits_to_show = fruits.loc[fruits_selected]
-##streamlit.dataframe(fruits_to_show)
-
-import streamlit as st
-import pandas as pd
-
-st.title("I love turtle")
-st.text("Turtles are green and live in ponds and lakes")
-
-# Load data and set 'Fruit' as index
 fruits = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruits = fruits.set_index('Fruit')
 
-# Get all unique fruits from the index
 all_fruits = fruits.index.unique().tolist()
 
-# Add 'Select All' option
-options = ['Select All'] + all_fruits
-
-# Create a multiselect widget for the fruits
-fruits_selected = st.multiselect("Pick some fruits:", options, [])
-
-# If 'Select All' is selected, select all fruits
-if 'Select All' in fruits_selected:
-    fruits_selected = all_fruits
-
-# Show the selected fruits' data
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(fruits.index), all_fruits)
 fruits_to_show = fruits.loc[fruits_selected]
-st.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
