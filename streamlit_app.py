3aab67a24fd14105b98975ead9abb206d82b69a2
Travor House <63683715+travorhouse12@users.noreply.github.com>
2023-10-25 20:51:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas as pd
 
 streamlit.title("I love turtle")
 streamlit.text("Turtles are green and live in a ponds and lakes")
+
+fruits = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(fruits)
